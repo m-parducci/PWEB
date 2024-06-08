55c0104cabf13f002538375529d74444d7eeba39
m-parducci <parduccimari@hotmail.com>
2024-06-08 16:29:21 -0300

    add pages and contents

--- ProjetoFinal/js/scripts.js
+++ ProjetoFinal/js/scripts.js
@@ -1,7 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('components/sidebar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector('.sidebar-container').innerHTML = data;
-        });
+$(document).ready(function() {
+    // Função para carregar o conteúdo da página de adicionar
+    function loadAddPage() {
+        $('.content').load('/pages/home.html');
+    }
+
+    // Carregar a página de adicionar por padrão
+    loadAddPage();
+
+    // Event listener para os links da barra lateral
+    $('.menu a').click(function(event) {
+        event.preventDefault();
+        var page = $(this).attr('href');
+        $('.content').load(page);
+    });
 });
